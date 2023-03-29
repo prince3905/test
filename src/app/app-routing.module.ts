4597c68605ece577bf90cee9b2465ffac5d376a1
prince3905 <praj@whr.loans>
2023-03-29 13:05:15 +0530

    testing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { Test2Component } from './test2/test2.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path:'',component:Test2Component},
+
+  { path:'test',component:Test2Component},
+
+//   { path: '', redirectTo: '/', pathMatch: 'full',
+//   children:[
+//     { path:'',redirectTo:'test',pathMatch:'full'},
+//     { path:'test',component:Test2Component},
+//   ] 
+// }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Test2Component } from './test2/test2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Test2Component
   ],
   imports: [
     BrowserModule,
